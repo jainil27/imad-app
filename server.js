45bd11d95dcfd97d72e5510006a749d4255a749c
jainil27 <jainil27@gmail.com>
2018-03-11 18:02:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+const { Pool, Client } = require('pg')
+
+const pool = new Pool({
+  user: 'jainil27',
+  host: 'db.imad.hasura.io',
+  database: 'jainil27',
+  password: process.env.DB_PASSWORD,
+  port: 5432,
+})
 
 var app = express();
 app.use(morgan('combined'));
@@ -23,6 +32,18 @@ app.get('/second', function(req,res){
 
 app.get('/third', function(req,res){
     res.send("Kavita Desai");
+});
+
+app.get('/test-db',function(req,res){
+    pool.query('SELECT * FROM test;',function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        }
+        else
+        {
+            res.send(JSON.stringify(result));
+        }
+    });
 });
 
 app.get('/ui/style.css', function (req, res) {
