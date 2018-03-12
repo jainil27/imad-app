373500a4f3952adfcec8e32c443d39da6bdc7d18
jainil27 <jainil27@gmail.com>
2018-03-12 16:00:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -36,16 +36,22 @@ app.get('/third', function(req,res){
 });
 
 var pool = new Pool(config);
-app.get('/test-db',function(req,res){
-    pool.query('SELECT * FROM test;',function(err,result){
-        if(err){
-            res.status(500).send(err.toString());
-        }
-        else
+app.get('/test-db',function(req,res)
+{
+	//make a query
+		//return a response
+	pool.query('select * from test',function(err,result) 
+    { 
+		if(err)
         {
-            res.send(JSON.stringify(result));
-        }
-    });
+			res.status(500).send(err.toString());
+		}
+		else
+        {
+			res.send(JSON.stringify(result.rows)); 
+		}
+	});
+
 });
 
 app.get('/ui/style.css', function (req, res) {
