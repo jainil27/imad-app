bb10e8a8481b25587bbc6ec3c4fa2ec9b7a22896
jainil27 <jainil27@gmail.com>
2018-03-12 15:55:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var Pool= require('pg').Pool;
 var config ={
   user: 'jainil27',
   database: 'jainil27',
-  host: 'db.imad.hasura.io',
+  host: 'db.imad.hasura.io:5432',
   port: '5432',
   password: process.env.DB_PASSWORD
 };
