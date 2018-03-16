d5da86617cca8a5f1f29fc03479a50d90e855a80
jainil27 <jainil27@gmail.com>
2018-03-16 20:13:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ app.get('/hash/:input', function(req, res){
   res.send(hashedString);
 });
 
-app.post('/create-user', function (req, res) {
+app.get('/create-user', function (req, res) {
    // username, password
    // {"username": "mohit", "password": "password"}
    // JSON
