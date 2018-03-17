8fa7d005ad6b40ec39b9d60d4a5305ddea17e808
jainil27 <jainil27@gmail.com>
2018-03-18 00:46:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,6 +28,6 @@ submit.onclick = function() {
     console.log(username);
     console.log(password);
     request.open('POST','http://jainil27.imad.hasura-app.io/login',true);
-    request.setRequestHeader('Content-Type','application/json');
-    request.send(JSON.stringify({username: username,password: password}));
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify({username: username, password: password}));
 };
