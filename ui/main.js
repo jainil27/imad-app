9866b4eecef83d4101a689edf2124156365bb518
jainil27 <jainil27@gmail.com>
2018-03-18 00:28:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,34 @@
 console.log('Loaded!');
+var submit= document.getElementById('submit_btn');
+submit.onClick = function() {
+    
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function(){
+        if(request.readystate === XMLHttpRequest.DONE)
+        {
+            if(requeststatus === 200){
+                console.log("user logged in");
+                alert("Log in success!!");
+            }
+            else if(requeststatus === 403)
+            {
+                alert("Incorrect password");
+            }
+            else if(requeststatus === 500)
+            {
+                alert("server Error");
+            }
+        }
+    };
+    
+    var nameip = document.getElementById('name');
+    var name = nameip.value;
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    request.open('POST','http://jainil27.imad.hasura-app.io/submit-name?name='+name,true);
+    request.setRequestHeader('Content-Type','application/json');
+    request.send(JSON.stringify({username:username,password:password}));
+};
